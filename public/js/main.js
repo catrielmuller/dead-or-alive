95fc0404f16b4760bcfe0eda072f85fa9ad103ad
Catriel Müller <catrielmuller@gmail.com>
2014-01-25 01:45:34 -0300

    FPS Controls

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,26 @@
 var app = {}
 
 app.init = function(){
+	
+    app.clock = new THREE.Clock();
+	app.scene = new THREE.Scene();
 
-    app.scene = new THREE.Scene();
     app.renderer = new THREE.WebGLRenderer();
     app.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(app.renderer.domElement);
 
-    app.requestPointerLock = app.renderer.domElement.requestPointerLock ||
-                 app.renderer.domElement.mozRequestPointerLock ||
-                 app.renderer.domElement.webkitRequestPointerLock;
+    app.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+    app.controls = new THREE.FirstPersonControls(app.camera);
+    app.controls.movementSpeed = 10;
+    app.controls.lookSpeed = 0.10;
+    app.controls.lookVertical = true;
+
+
+    app.requestPointerLock = app.controls.domElement.requestPointerLock ||
+                 app.controls.domElement.mozRequestPointerLock ||
+                 app.controls.domElement.webkitRequestPointerLock;
+
 
     app.exitPointerLock = document.exitPointerLock ||
                 document.mozExitPointerLock ||
@@ -17,16 +28,16 @@ app.init = function(){
 
     app.renderer.domElement.onclick = app.requestPointerLock;
 
-    app.keyboard = new THREEx.KeyboardState();
 
-    app.camera = new Camera();
+
     app.cube = new Cube();
 
     app.scene.add(app.cube.cube);
 
-    app.camera.camera.position.z = 5;
+    app.camera.position.z = 5;
 
     app.render();
+    app.renderer.domElement.webkitRequestPointerLock()
 };
 
 app.animate = function(){
@@ -35,36 +46,30 @@ app.animate = function(){
     app.update();
 }
 
+
 app.update = function(){
-    if( app.keyboard.pressed("a") ){
+    var delta = app.clock.getDelta();
+    app.controls.update(delta);
+
+    /*
+    if( app.keyboard.pressed("1") ){
         app.cube.rotateLeft();
     }
-    if( app.keyboard.pressed("d") ){
+    if( app.keyboard.pressed("2") ){
         app.cube.rotateRight();
     }
-    if( app.keyboard.pressed("w") ){
+    if( app.keyboard.pressed("3") ){
         app.cube.rotateUp();
     }
-    if( app.keyboard.pressed("s") ){
+    if( app.keyboard.pressed("4") ){
         app.cube.rotateDown();
     }
-
-    if( app.keyboard.pressed("left") ){
-        app.camera.moveLeft();
-    }
-    if( app.keyboard.pressed("right") ){
-        app.camera.moveRight();
-    }
-    if( app.keyboard.pressed("up") ){
-        app.camera.moveUp();
-    }
-    if( app.keyboard.pressed("down") ){
-        app.camera.moveDown();
-    }
+    */
+    
 }
 
 app.render = function() {
-    app.renderer.render(app.scene, app.camera.camera);
+    app.renderer.render(app.scene, app.camera);
 }
 
 
